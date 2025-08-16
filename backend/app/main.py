a97dab265a46e3d59aac910eac93b95f7980bb33
Alejandro Alonso <alejandroalonso@Mac.lan>
2025-08-15 23:04:02 -0400

    versioning

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,13 +4,24 @@ from app.routers import market_data
 from app.routers import portfolios
 
 
+TITLE = "Oscillo Backend API"
+SEM_VER = "1.0"
+DESCRIPTION = "Developer API for Oscillo, a portfolio tracking & paper trading platform."
+
+
 def build_app():
-    app = FastAPI(
-        title="Oscillo Backend API"
+    app = FastAPI()
+
+    api = FastAPI(
+        title=TITLE,
+        version=SEM_VER,
+        description=DESCRIPTION,
     )
 
     # Include routes
-    app.include_router(market_data.router)
-    app.include_router(portfolios.router)
+    api.include_router(market_data.router)
+    api.include_router(portfolios.router)
+
+    app.mount(f"/api/{SEM_VER}/", api)
 
     return app
