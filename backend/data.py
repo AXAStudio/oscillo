b31a8b9164ac7b8def902cdca5b4045eb98d4a25
Andres Alonso <108694498+AXAStudio@users.noreply.github.com>
2025-08-17 12:27:15 -0400

    artificial testing data

--- backend/data.py
+++ backend/data.py
@@ -1,27 +1,178 @@
 orders = [
     {
-        'timestamp': '2025-08-16T17:38:12.666823+00:00',
+        'timestamp': '2025-08-11T13:35:12.666823+00:00',
         'ticker': 'CA$H',
         'quantity': 10000,
         'price': 1,
-        'portfolio_id':
-        '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
         'order_id': 'b5d7b09c-2b0a-4d1d-9f20-442ffca380e8'
     },
     {
-        'timestamp': '2025-08-16T17:56:35.351519+00:00',
+        'timestamp': '2025-08-12T14:56:35.351519+00:00',
         'ticker': 'AAPL',
         'quantity': 2,
         'price': 670.67,
         'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
         'order_id': 'e2b80b05-eb4e-4f92-9e9e-f21461fdae32'
-    }, 
+    },
     {
-        'timestamp': '2025-08-16T17:58:54.442574+00:00',
+        'timestamp': '2025-08-13T15:58:54.442574+00:00',
         'ticker': 'TSLA',
         'quantity': 10,
         'price': 200.25,
         'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
         'order_id': '3c9db0c7-6de8-48d2-845e-247d7f3fb091'
+    },
+    {
+        'timestamp': '2025-08-11T19:00:05.123456+00:00',
+        'ticker': 'AMZN',
+        'quantity': 3,
+        'price': 190.05,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': '0a1b2c3d-4e5f-4a67-9abc-def012345678'
+    },
+    {
+        'timestamp': '2025-08-12T13:35:10.987654+00:00',
+        'ticker': 'XOM',
+        'quantity': 8,
+        'price': 114.33,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': '1b2c3d4e-5f60-4b78-8bcd-ef0123456789'
+    },
+    {
+        'timestamp': '2025-08-12T19:30:45.000321+00:00',
+        'ticker': 'GOOGL',
+        'quantity': 4,
+        'price': 163.20,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': '2c3d4e5f-6071-4c89-7cde-f0123456789a'
+    },
+    {
+        'timestamp': '2025-08-13T16:20:30.654321+00:00',
+        'ticker': 'MSFT',
+        'quantity': 5,
+        'price': 430.10,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': '3d4e5f60-7182-4d9a-6def-0123456789ab'
+    },
+    {
+        'timestamp': '2025-08-14T13:40:12.111111+00:00',
+        'ticker': 'VOO',
+        'quantity': 3,
+        'price': 512.75,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': '4e5f6071-8293-4eab-5ef0-123456789abc'
+    },
+    {
+        'timestamp': '2025-08-14T18:45:59.222222+00:00',
+        'ticker': 'NVDA',
+        'quantity': 1,
+        'price': 120.50,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': '5f607182-93a4-4fbc-4f01-23456789abcd'
+    },
+    {
+        'timestamp': '2025-08-15T14:55:05.333333+00:00',
+        'ticker': 'JPM',
+        'quantity': 5,
+        'price': 210.45,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': '60718293-a4b5-40cd-3f12-3456789abcde'
+    },
+    {
+        'timestamp': '2025-08-15T19:10:22.555555+00:00',
+        'ticker': 'KO',
+        'quantity': 12,
+        'price': 63.71,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': '8293a4b5-c6d7-42ef-1d34-56789abcdef0'
+    },
+    {
+        'timestamp': '2025-08-15T19:59:30.444444+00:00',
+        'ticker': 'META',
+        'quantity': 2,
+        'price': 470.12,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': '718293a4-b5c6-41de-2e23-456789abcdef'
+    },
+    {
+        'timestamp': '2025-08-18T14:10:00+00:00',
+        'ticker': 'AAPL',
+        'quantity': -1,
+        'price': 675.20,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': 'a1e2f3b4-5678-49aa-9cde-001122334455'
+    },
+    {
+        'timestamp': '2025-08-18T19:50:33+00:00',
+        'ticker': 'TSLA',
+        'quantity': -4,
+        'price': 202.15,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': 'b2f3a4c5-6789-40bb-adde-112233445566'
+    },
+    {
+        'timestamp': '2025-08-19T15:05:45+00:00',
+        'ticker': 'NVDA',
+        'quantity': -1,
+        'price': 121.30,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': 'c3a4b5d6-7890-41cc-bbee-223344556677'
+    },
+    {
+        'timestamp': '2025-08-19T19:40:10+00:00',
+        'ticker': 'VOO',
+        'quantity': -1,
+        'price': 514.90,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': 'd4b5c6e7-8901-42dd-ccff-334455667788'
+    },
+    {
+        'timestamp': '2025-08-20T13:45:12+00:00',
+        'ticker': 'MSFT',
+        'quantity': -2,
+        'price': 431.25,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': 'e5c6d7f8-9012-43ee-dd00-445566778899'
+    },
+    {
+        'timestamp': '2025-08-20T18:59:59+00:00',
+        'ticker': 'GOOGL',
+        'quantity': -1,
+        'price': 164.00,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': 'f6d7e809-0123-44ff-ee11-5566778899aa'
+    },
+    {
+        'timestamp': '2025-08-21T14:30:06+00:00',
+        'ticker': 'KO',
+        'quantity': -5,
+        'price': 64.10,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': '07e8f91a-1234-4500-ff22-66778899aabb'
+    },
+    {
+        'timestamp': '2025-08-21T19:15:22+00:00',
+        'ticker': 'AMZN',
+        'quantity': -1,
+        'price': 189.70,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': '18f9a02b-2345-4611-0011-778899aabbcc'
+    },
+    {
+        'timestamp': '2025-08-22T13:35:31+00:00',
+        'ticker': 'TSLA',
+        'quantity': -3,
+        'price': 203.05,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': '290ab13c-3456-4722-1122-8899aabbccdd'
+    },
+    {
+        'timestamp': '2025-08-22T19:58:48+00:00',
+        'ticker': 'META',
+        'quantity': -1,
+        'price': 471.50,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': '3a1bc24d-4567-4833-2233-99aabbccdde0'
     }
 ]
