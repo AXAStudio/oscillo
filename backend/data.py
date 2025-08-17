c4b4d45ce23b0a54ba31b9724026c19dde146f4f
Alejandro Alonso <alejandroalonso@Mac.lan>
2025-08-17 12:21:59 -0400

    test data

---
+++ backend/data.py
@@ -0,0 +1,27 @@
+orders = [
+    {
+        'timestamp': '2025-08-16T17:38:12.666823+00:00',
+        'ticker': 'CA$H',
+        'quantity': 10000,
+        'price': 1,
+        'portfolio_id':
+        '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': 'b5d7b09c-2b0a-4d1d-9f20-442ffca380e8'
+    },
+    {
+        'timestamp': '2025-08-16T17:56:35.351519+00:00',
+        'ticker': 'AAPL',
+        'quantity': 2,
+        'price': 670.67,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': 'e2b80b05-eb4e-4f92-9e9e-f21461fdae32'
+    }, 
+    {
+        'timestamp': '2025-08-16T17:58:54.442574+00:00',
+        'ticker': 'TSLA',
+        'quantity': 10,
+        'price': 200.25,
+        'portfolio_id': '6821b1c8-6f21-42cc-bad3-0a1bf6a1cf86',
+        'order_id': '3c9db0c7-6de8-48d2-845e-247d7f3fb091'
+    }
+]
