5b25a02e78ac691eda0f05bc75a190bfc3c95682
Alejandro Alonso <alejandroalonso@Mac.lan>
2025-08-18 00:05:10 -0400

    pf_agg temp removal

--- backend/app/services/portfolios.py
+++ backend/app/services/portfolios.py
@@ -8,11 +8,10 @@ from datetime import datetime
 from supabase import create_client
 
 from app.configs import config
-from app.models import Portfolio, Order, Positions
 from app.utils.logger import setup_logger
-from app.services.pf_agg import get_portfolio_history
-from app.services.positions import get_portfolio_positions
+from app.models import Portfolio, Order, Positions
 from app.services.market import fetch_recent_quotes
+from app.services.positions import get_portfolio_positions
 
 
 _logger = setup_logger()
